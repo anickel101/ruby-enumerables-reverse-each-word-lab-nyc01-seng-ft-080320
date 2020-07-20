1d17d7712b34d71e33b75f1a1fef5b4a130b6bdf
Anson Nickel <anson.nickel@gmail.com>
2020-07-20 15:31:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,3 +9,11 @@ def reverse_each_word(string)
   reverse_string_array.join(" ")
 end
 
+#def reverse_each_word(string)
+#  reverse_string_array = []
+#  temp_string = string.split
+#  temp_string.each do |word|
+#    reverse_string_array << word.reverse
+#  end
+#  reverse_string_array.join(" ")
+#end
