a75226edc59db1722996aab24003beec121948f5
Anson Nickel <anson.nickel@gmail.com>
2020-07-20 15:36:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def reverse_each_word(string)
-  string.split.collect {|word| word.reverse}
+  reverse_string = string.split.collect {|word| word.reverse}
   reverse_array.join(" ")
 end
 
