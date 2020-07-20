32f8375d5537a5fb0f74c101fe54b9808062ad42
Anson Nickel <anson.nickel@gmail.com>
2020-07-20 15:18:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,10 @@
 require 'pry'
 
 def reverse_each_word(string)
-  string.split.each {|x| x.reverse}
+  reverse = ""
+  string.split.each do |x| 
+    reverse << x.reverse
+  end
 end
 
 binding.pry
